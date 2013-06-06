3dc96dbb817348c8e805760cdf1d2e16ef70ff06
Héctor <hector0193@gmail.com>
2013-06-06 17:43:27 +0200

    Initial commit.

---
+++ src/manticore/example/ManticoreExample.java
@@ -0,0 +1,17 @@
+package manticore.example;
+
+/**
+ *
+ * @author hector
+ */
+public class ManticoreExample
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
